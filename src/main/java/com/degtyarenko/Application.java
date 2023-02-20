f2b7b85d986a2febb2593107d28edb4ebd103719
xomkins <xomkinss@gmail.com>
2023-02-20 10:47:21 +0300

    delete test

--- src/main/java/com/degtyarenko/Application.java
+++ src/main/java/com/degtyarenko/Application.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
     }
 
 }
