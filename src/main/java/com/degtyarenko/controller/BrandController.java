ffccb0dadc83f9a36f554ea26f982aead38bd850
Volha Dzehtsiarenka <volha_dzehtsiarenka@senla.eu>
2023-07-12 17:31:44 +0300

    add spring spring_cloud

--- src/main/java/com/degtyarenko/controller/BrandController.java
+++ src/main/java/com/degtyarenko/controller/BrandController.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -46,6 +47,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  * @version 1.0
  * @since 2022-12-22
  */
+@RefreshScope
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/brand")
